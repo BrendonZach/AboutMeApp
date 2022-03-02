4ea22240967a21e6e6eaee6e906b6594d986d6c8
Brendon Zach <brendon_zach@outlook.com>
2022-03-02 15:37:21 -0700

    ADDED lots of code and segmented controller and added outlets and actions.

--- AboutMeApp/ViewController.swift
+++ AboutMeApp/ViewController.swift
@@ -7,12 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
+    @IBAction func changePicture(_ sender: Any) {
+    }
+    
+    @IBAction func segmentAction(_ sender: Any) {
+        
+        switch segmentOutlet.selectedSegmentIndex
+        {
+        case 0:
+            segmentText.text = "The personality that i got after the 16 personality tests was ISTP-T "
+        case 1:
+            segmentText.text = "The personality that i got after the 16 personality tests was ISTP-T "
+        case 2:
+            segmentText.text = "The personality that i got after the 16 personality tests was ISTP-T "
+        default:
+            segmentText.text = "The personality that i got after the 16 personality tests was ISTP-T "
+        }
+    }
+    
+    @IBOutlet weak var segmentOutlet: UISegmentedControl!
+    
+    
+    @IBOutlet weak var segmentText: UITextView!
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
 
 
 }
