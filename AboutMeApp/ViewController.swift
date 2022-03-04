6e98426fd6508672350d080fda8dfe88e1c97da8
Brendon Zach <brendon_zach@outlook.com>
2022-03-04 12:50:02 -0700

    Added things for the segment outlet

--- AboutMeApp/ViewController.swift
+++ AboutMeApp/ViewController.swift
@@ -18,13 +18,13 @@ class ViewController: UIViewController
         switch segmentOutlet.selectedSegmentIndex
         {
         case 0:
-            segmentText.text = "The personality that i got after the 16 personality tests was ISTP-T "
+            segmentText.text = "The personality that i got after the 16 personality tests was Virtuoso ISTP-T this personality is people who love exploring with their hands and their eyes people with this personality type are natural makers moving from project to project. "
         case 1:
-            segmentText.text = "The personality that i got after the 16 personality tests was ISTP-T "
+            segmentText.text = "My tendency after I took this test was that I am an obliger, obligers are people who meet outer expectations but struggle to meet inner expectations and they are motivated by external accountablitiy they wake up and think what must I do today.  I think that this is a very accurate thing because I dont usually hold myself accountable for things, however when i am being held accountable by other people I usually can get things done."
         case 2:
-            segmentText.text = "The personality that i got after the 16 personality tests was ISTP-T "
+            segmentText.text = "My color personality was "
         default:
-            segmentText.text = "The personality that i got after the 16 personality tests was ISTP-T "
+            segmentText.text = "The personality that i got after the 16 personality tests was Virtuoso ISTP-T this personality is people who love exploring with their hands and their eyes people with this personality type are natural makers moving from project to project.  "
         }
     }
     
